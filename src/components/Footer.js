a1e7a010681dfa3277e7b7c1a4208530463dd088
smirnoffnew <smirnoffnew@gmail.com>
2020-12-23 18:34:52 +0200

    implement base components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>{new Date().getFullYear()}</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { auth } from "../config/firebase";
+import "../styles/Header.css";
+
+function Header({ user }) {
+  const handleAuth = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
+
+  return (
+    <div className="header">
+      <div className="header__nav">
+        <div className="header__option" onClick={handleAuth}>
+          <span className="header__option__firstLine">Hello {user?.email}</span>
+          <span className="header__option__secondLine">
+            Sign Out
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
