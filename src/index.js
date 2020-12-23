2a5570c6f82f926bab5f0f85a0b331553fb4c93e
smirnoffnew <smirnoffnew@gmail.com>
2020-12-23 18:25:35 +0200

    set StateProvider for App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import { StateProvider } from "./store/context/StateProvider";
+import reducer, { initialState } from "./store/reducer/reducer";
+import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  document.getElementById("root")
+)
